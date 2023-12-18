32f026f69b6e7b417220815c17e320f7feb95f07
EkaterinaKochneva <kochnevaes@gmail.com>
2023-12-18 18:04:55 +0300

    fix: remove app require

--- gulp/tasks/favicon.js
+++ gulp/tasks/favicon.js
@@ -1,7 +1,6 @@
 const { src, dest } = require('gulp');
 
 const path = require('../config/path.js');
-const app = require('../config/app.js');
 
 module.exports = function favicon() {
   return src(path.favicon.src)
